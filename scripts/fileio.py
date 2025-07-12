550f222aa560f3053ac2b129edad3884abd70f7a
dormant-user <svignesh1793@gmail.com>
2025-07-11 22:46:29 -0500

    Add instructions for self restart instructions
    Run linter on dev scripts

--- scripts/fileio.py
+++ scripts/fileio.py
@@ -3,15 +3,15 @@ import os
 import pathlib
 from urllib.parse import quote
 
-from init import urljoin, NINJA_API_URL, NINJA_API_TIMEOUT, SESSION
+from init import NINJA_API_TIMEOUT, NINJA_API_URL, SESSION, urljoin
 
 
 def run_command(command: str, timeout: int = NINJA_API_TIMEOUT) -> dict:
     """Runs a command on the Ninja API.
 
     Args:
-        command (str): The command to run.
-        timeout (int): Timeout for the command execution in seconds.
+        command: The command to run.
+        timeout: Timeout for the command execution in seconds.
 
     Returns:
         dict: A dictionary containing the command output and error messages.
@@ -34,55 +34,58 @@ def get_current_working_directory() -> str:
 
 
 def upload_file(
-        filepath: str,
-        destination: str = None,
-        overwrite: bool = False
+    filepath: str, destination: str = None, overwrite: bool = False
 ) -> None:
     """Uploads a file to the Ninja API.
 
     Args:
-        filepath (str): Path to the file to upload.
-        destination (str, optional): Destination directory on the server. Defaults to the current working directory.
-        overwrite (bool, optional): Whether to overwrite the file if it already exists. Defaults to False.
+        filepath: Path to the file to upload.
+        destination: Destination directory on the server. Defaults to the current working directory.
+        overwrite: Whether to overwrite the file if it already exists. Defaults to False.
     """
     url = urljoin(
         NINJA_API_URL,
         f"/put-file?directory={quote(destination) if destination else quote(get_current_working_directory())}"
-        f"&overwrite={'true' if overwrite else 'false'}"
+        f"&overwrite={'true' if overwrite else 'false'}",
     )
     assert os.path.isfile(filepath), f"File {filepath} does not exist"
     with open(filepath, "rb") as f:
-        files = {"file": (os.path.basename(filepath), f), "type": mimetypes.guess_type(filepath)[0]}
+        files = {
+            "file": (os.path.basename(filepath), f),
+            "type": mimetypes.guess_type(filepath)[0],
+        }
         response = SESSION.put(url, files=files)
         assert response.ok, response.text
         print(response.json())
 
 
 def delete_content(
-        filepath: str = None,
-        directory: str = None,
-        recursive: bool = False
+    filepath: str = None, directory: str = None, recursive: bool = False
 ) -> None:
     """Deletes a file or directory from the Ninja API.
 
     Args:
-        filepath (str): Path to the file or directory to delete.
-        directory (str, optional): Directory containing the file. Defaults to None.
-        recursive (bool, optional): Whether to delete directories recursively. Defaults to False.
+        filepath: Path to the file or directory to delete.
+        directory: Directory containing the file. Defaults to None.
+        recursive: Whether to delete directories recursively. Defaults to False.
     """
     assert any((filepath, directory)), "Either filepath or directory must be provided"
     if directory and recursive:
-        flag = input("Both directory path and recursive are set to True. Do you want to continue? [y/N]\n")
+        flag = input(
+            "Both directory path and recursive are set to True. Do you want to continue? [y/N]\n"
+        )
         if flag != "y":
             return
         print(f"Deleting directory: {directory!r} and all it's subdirectories.")
     url = urljoin(NINJA_API_URL, "/delete-content")
-    response = SESSION.delete(url, json={"filepath": filepath, "directory": directory, "recursive": recursive})
+    response = SESSION.delete(
+        url, json={"filepath": filepath, "directory": directory, "recursive": recursive}
+    )
     response.raise_for_status()
     print(response.json())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pathlib.Path("keep").touch(exist_ok=True)
     upload_file("keep", overwrite=True)
     delete_content("keep")
--- scripts/zipper.py
+++ scripts/zipper.py
@@ -5,8 +5,7 @@ import zipfile
 
 
 def archive(
-        path: pathlib.Path,
-        directory: pathlib.Path = pathlib.Path(__file__).parent
+    path: pathlib.Path, directory: pathlib.Path = pathlib.Path(__file__).parent
 ) -> pathlib.Path:
     """Archives a file or directory into a zip file.
 
@@ -19,7 +18,7 @@ def archive(
         The path to the created zip file.
     """
     zip_file = directory / f"{path.stem}.zip"
-    with zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(zip_file, "w", zipfile.ZIP_DEFLATED) as zipf:
         if path.is_dir():
             for root, _, files in os.walk(path):
                 for file in files:
@@ -32,8 +31,7 @@ def archive(
 
 
 def unarchive(
-        zip_file: pathlib.Path,
-        directory: pathlib.Path = pathlib.Path(__file__).parent
+    zip_file: pathlib.Path, directory: pathlib.Path = pathlib.Path(__file__).parent
 ) -> pathlib.Path:
     """Unarchives a zip file into a specified directory.
 
@@ -47,8 +45,10 @@ def unarchive(
     """
     if not zip_file.exists():
         raise FileNotFoundError(f"Zip file {zip_file} does not exist.")
-    if not zip_file.suffix in ['.zip', '.tar', '.tar.gz', '.tar.bz2']:
-        raise ValueError("Unsupported archive format. Only .zip, .tar, .tar.gz, and .tar.bz2 are supported.")
+    if zip_file.suffix not in [".zip", ".tar", ".tar.gz", ".tar.bz2"]:
+        raise ValueError(
+            "Unsupported archive format. Only .zip, .tar, .tar.gz, and .tar.bz2 are supported."
+        )
     # Create a destination directory for unzipped content
     directory.mkdir(parents=True, exist_ok=True)
     shutil.unpack_archive(zip_file, directory)
